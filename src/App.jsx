d3d135a8f07c9d4252e546380ed5919de4403dc5
lyszt <kalliddel@gmail.com>
2025-07-09 21:51:12 -0300

    Entry animation complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef } from 'react';
 import MainNav from './nav.jsx';
-import './App.css'
+import './App.css';
 
 function App() {
+    const titleRef = useRef();
 
-  return (
-    <main>
-        <MainNav/>
-        <div className="w-full text-center flex flex-col gap-12 justify-center items-center">
-            <h1 className="font-bold font-title text-gray-600 text-shadow-2xs shadow-gray-900 text-8xl"> La simplicité est la <br></br>
-                <b className="text-gray-700"> sophistication suprême.</b> </h1>
-            <div className="w-40  brightness-100
-             shadow-sm from-gray-700 to-gray-800 h-2 text-center duration-200 rounded-full">
+    function revealWords(reference) {
+        const element = reference.current;
+        if (!element) return;
+
+        const text = element.textContent;
+        element.textContent = "";
+        [...text].forEach((char, index) => {
+            setTimeout(() => {
+                element.textContent += char;
+            }, index * 100);
+        });
+    }
+
+    useEffect(() => {
+        revealWords(titleRef);
+    }, []);
+
+    return (
+        <main className="z-10">
+            <MainNav />
+            <div className="w-full text-center flex flex-col gap-12 justify-center items-center">
+                <h1 ref={titleRef} className="font-bold font-title text-gray-600 text-shadow-2xs shadow-gray-900 text-8xl">
+                    La simplicité est la <br />
+                    <b className="text-gray-700">sophistication suprême.</b>
+                </h1>
+                <div className="w-40 brightness-100 shadow-sm bg-gradient-to-r from-gray-700 to-gray-500 h-2 text-center duration-200 rounded-full div-bar">
+                </div>
             </div>
-        </div>
-    </main>
-  )
+        </main>
+    );
 }
 
-export default App
+export default App;
