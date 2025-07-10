ea6e7e1fe7ca9ff7258b9f5d83bf39672bb35708
lyszt <kalliddel@gmail.com>
2025-07-10 13:56:02 -0300

    improves speed of code by changing vanilla js element content update to react's usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import MainNav from './nav.jsx';
 import './App.css';
 
 function App() {
-    const titleRef = useRef();
+    const [wordState, setWordState] = useState("");
 
-    function revealWords(reference) {
-        const element = reference.current;
-        if (!element) return;
-
-        const text = element.textContent;
-        element.textContent = "";
-        [...text].forEach((char, index) => {
-            setTimeout(() => {
-                element.textContent += char;
-            }, index * 100);
-        });
-    }
 
     useEffect(() => {
-        revealWords(titleRef);
-    }, []);
+        setWordState("");
+        const textInput = "La simplicité est la sophistication suprême.";
+        const timeouts = [];
 
+
+        [...textInput].forEach((char, index) => {
+            const timeout = setTimeout(() => {
+                setWordState((prev) => prev + char);
+            }, index * 100);
+            timeouts.push(timeout);
+        });
+
+        return () => {
+            timeouts.forEach(clearTimeout);
+        };
+    }, []);
     return (
         <main className="z-10">
             <MainNav />
             <div className="w-full text-center flex flex-col gap-12 justify-center items-center">
-                <h1 ref={titleRef} className="font-bold font-title text-gray-600 text-shadow-2xs shadow-gray-900 text-8xl">
-                    La simplicité est la <br />
-                    <b className="text-gray-700">sophistication suprême.</b>
+                <h1 className="font-bold font-title text-gray-600 text-shadow-2xs shadow-gray-900 text-8xl">
+                    {wordState}
                 </h1>
                 <div className="w-40 brightness-100 shadow-sm bg-gradient-to-r from-gray-700 to-gray-500 h-2 text-center duration-200 rounded-full div-bar">
                 </div>
