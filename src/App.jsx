6d734d4e40932f843a531a98a8603007f7c559bf
abensoft-joaoluis <joao.santos@abensoft.com.br>
2025-08-09 17:57:46 -0300

    make animation horizontal

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,9 @@ function App() {
                 {activePage === 'home' && (
                     <motion.div
                         key="home"
-                        initial={{ opacity: 0, y: '-100%' }}
-                        animate={{ opacity: 1, y: '0%' }}
-                        exit={{ opacity: 0, y: '100%' }}
+                        initial={{ opacity: 0, x: '-100%' }}
+                        animate={{ opacity: 1, x: '0%' }}
+                        exit={{ opacity: 0, x: '100%' }}
                         transition={{ duration: 0.5 }}
                     >
                         <Intro />
