316d51366037cf367c975b5e9313256bd37be6e8
lyszt <kalliddel@gmail.com>
2025-09-04 11:03:33 -0300

    adds children of lumiere from expedition 33

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -3,14 +3,15 @@ import { useTranslation } from 'react-i18next';
 import playIcon from './assets/icons/play.svg';
 import pauseIcon from './assets/icons/pause.svg';
 import standByMeSong from './assets/audios/standbyme.mp3';
+import childrenoflumiere from './assets/audios/childrenoflumiere.mp3'
 
 const MusicPlayer = () => {
     const { t } = useTranslation();
     const [isPlaying, setIsPlaying] = useState(false);
     const [currentTime, setCurrentTime] = useState(0);
     const audioPlayer = useRef(null);
-    const songName = "Stand By Me";
-    const artistName = "Florence + The Machine";
+    const songName = "Children of Lumière";
+    const artistName = "Clair Obscur: Expedition 33";
 
     const handleTimeUpdate = () => {
         if (audioPlayer.current) {
@@ -44,7 +45,7 @@ const MusicPlayer = () => {
                 <button onClick={toggleMusicTheme} className="inline-flex items-center justify-center gap-3 p-3">
                     <img src={isPlaying ? pauseIcon : playIcon} className="w-4 text-stone-700 invert" alt={t('playMusicAlt')} />
                     <audio ref={audioPlayer} onTimeUpdate={handleTimeUpdate} onEnded={handleSongEnd}>
-                        <source src={standByMeSong} type="audio/mpeg" />
+                        <source src={childrenoflumiere} type="audio/mpeg" />
                     </audio>
                 </button>
             </div>
